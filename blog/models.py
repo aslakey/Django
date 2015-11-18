ae79319a36aa66d0a376558d613c01c724afa73d
Austin Slakey <austin.slakey@columbia.edu>
2015-11-17 22:56:01 -0500

    Django test first commit

---
+++ blog/models.py
@@ -0,0 +1,13 @@
+from django.db import models
+from django.utils import timezone
+
+# Create your models here.
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
